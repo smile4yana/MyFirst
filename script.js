e6301916b8b4d62830253de3458602a4dc0b6221
Yana <example@gmail.com>
2020-09-02 08:45:32 +0300

    Lesson12

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-/*jslint node: true */
-"use strict";
+let numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?','');
 
-// const category = 'toys';
-// console.log(`https://someurl.com/toys/${category}/5');
+const personalMovieDB={
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-let incr = 10,
-    decr = 10;
-   
+const 
+a = prompt('Один из последних просмотренных фильмов?',''),
+b = prompt('На сколько оцените его?',''),
+c = prompt('Один из последних просмотренных фильмов?',''),
+d = prompt('На сколько оцените его?','');
 
-    console.log(incr++);
-    console.log(decr++);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
